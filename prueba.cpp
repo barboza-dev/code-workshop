e2ae23f809ac8407a57b4847032a0b7cf0d8567e
barboza-raul <raulbarbozablanco@gmail.com>
2025-08-22 11:44:04 -0500

    Se crea archivo y modifica prueba

---
+++ prueba.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, world!" << std::endl;
+    int number;
+    std::cout << "Enter a number: ";
+    std::cin >> number;
+    std::cout << "You entered: " << number << std::endl;
+    return 0;
+}
